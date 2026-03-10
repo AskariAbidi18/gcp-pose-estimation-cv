a05150611fe0b33defbc7bafaade7ba539e26805
Syed Mohammad Askari Abidi <askari.abidi.2005@gmail.com>
2026-03-10 11:59:38 +0530

    feat created dataloader

---
+++ dataset/gcp_dataset.py
@@ -0,0 +1,74 @@
+import os
+import json
+import cv2
+import torch
+from torch.utils.data import Dataset
+
+
+class GCPDataset(Dataset):
+    def __init__(self, root_dir, img_size=512, transform=None):
+        """
+        root_dir: path to train_dataset
+        img_size: resized image size
+        transform: albumentations transforms
+        """
+
+        self.root_dir = root_dir
+        self.img_size = img_size
+        self.transform = transform
+
+        label_path = os.path.join(root_dir, "curated_gcp_marks.json")
+
+        with open(label_path) as f:
+            self.labels = json.load(f)
+
+        self.image_paths = list(self.labels.keys())
+
+        self.shape_map = {
+            "Cross": 0,
+            "Square": 1,
+            "L-Shaped": 2
+        }
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+
+        rel_path = self.image_paths[idx]
+        label = self.labels[rel_path]
+
+        img_path = os.path.join(self.root_dir, rel_path)
+
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        h, w = image.shape[:2]
+
+        x = label["mark"]["x"]
+        y = label["mark"]["y"]
+
+        shape = label["verified_shape"]
+        shape_label = self.shape_map[shape]
+
+        # resize image
+        image = cv2.resize(image, (self.img_size, self.img_size))
+
+        # scale coordinates
+        scale_x = self.img_size / w
+        scale_y = self.img_size / h
+
+        x = x * scale_x
+        y = y * scale_y
+
+        if self.transform:
+            augmented = self.transform(image=image, keypoints=[(x, y)])
+            image = augmented["image"]
+            x, y = augmented["keypoints"][0]
+
+        image = torch.tensor(image).permute(2, 0, 1).float() / 255.0
+
+        keypoint = torch.tensor([x, y]).float()
+        shape_label = torch.tensor(shape_label).long()
+
+        return image, keypoint, shape_label
