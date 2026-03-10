615e66d06e0bc4ce18e4017230e751e773f26dff
Syed Mohammad Askari Abidi <askari.abidi.2005@gmail.com>
2026-03-10 12:29:20 +0530

    feat : model training

---
+++ train.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from dataset.gcp_dataset import GCPDataset
+from dataset.transforms import get_train_transforms
+from models.gcp_model import GCPModel
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+dataset = GCPDataset(
+    "/content/GCP_Assignment_Datasets/train_dataset",
+    transform=get_train_transforms(512)
+)
+
+loader = DataLoader(dataset, batch_size=16, shuffle=True)
+
+
+model = GCPModel().to(device)
+
+
+keypoint_loss = nn.MSELoss()
+shape_loss = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
+
+
+EPOCHS = 25
+
+
+for epoch in range(EPOCHS):
+
+    model.train()
+
+    total_loss = 0
+
+    for images, keypoints, labels in loader:
+
+        images = images.to(device)
+        keypoints = keypoints.to(device)
+        labels = labels.to(device)
+
+        pred_kp, pred_shape = model(images)
+
+        loss_kp = keypoint_loss(pred_kp, keypoints)
+        loss_shape = shape_loss(pred_shape, labels)
+
+        loss = loss_kp + loss_shape
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    print(f"Epoch {epoch+1} Loss {total_loss:.4f}")
+
+
+torch.save(model.state_dict(), "gcp_model.pth")
