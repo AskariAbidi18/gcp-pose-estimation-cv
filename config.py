37f122787abdf3fa92e8264b7a6660661dcefa2c
Syed Mohammad Askari Abidi <askari.abidi.2005@gmail.com>
2026-03-10 11:49:10 +0530

    chore : made the config

---
+++ config.py
@@ -0,0 +1,7 @@
+TRAIN_DATASET_PATH = "/content/GCP_Assignment_Datasets/train_dataset"
+TEST_DATASET_PATH = "/content/GCP_Assignment_Datasets/test_dataset"
+
+IMG_SIZE = 512
+BATCH_SIZE = 16
+EPOCHS = 25
+LR = 3e-4
